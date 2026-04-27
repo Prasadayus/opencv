833b5df719a34e277e7420616c2b9157f0811189
Prasad Ayush Kumar <22bcs191@iiitdmj.ac.in>
2026-04-27 17:31:59 +0530

    Make the Tokenizer class access to llm also

---
+++ modules/dnn/src/tokenizer/tokenizer_impl.hpp
@@ -0,0 +1,22 @@
+// This file is part of OpenCV project.
+// It is subject to the license terms in the LICENSE file found in the top-level directory
+// of this distribution and at http://opencv.org/license.html.
+
+#ifndef OPENCV_DNN_TOKENIZER_IMPL_HPP
+#define OPENCV_DNN_TOKENIZER_IMPL_HPP
+
+#include <opencv2/dnn/dnn.hpp>
+
+namespace cv { namespace dnn {
+CV__DNN_INLINE_NS_BEGIN
+
+struct Tokenizer::Impl {
+    virtual ~Impl() {}
+    virtual std::vector<int> encode(const std::string& text) = 0;
+    virtual std::string decode(const std::vector<int>& tokens) = 0;
+};
+
+CV__DNN_INLINE_NS_END
+}}
+
+#endif // OPENCV_DNN_TOKENIZER_IMPL_HPP
